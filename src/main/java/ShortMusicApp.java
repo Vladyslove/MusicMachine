da6744c4ca53e5cec5471b112fbf6aff7e62c532
Vladyslove <od.vladyslav@gmail.com>
2017-09-05 01:23:07 +0300

    Added new Sequence and got a new Track from the Sequence

--- src/main/java/ShortMusicApp.java
+++ src/main/java/ShortMusicApp.java
@@ -1,6 +1,4 @@
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Sequencer;
+import javax.sound.midi.*;
 
 /**
  * Created by User on 05.09.2017.
@@ -13,9 +11,21 @@ public class ShortMusicApp {
 
     public void play() {
         try {
+            /*The Sequencer is the thing that actually causes a song to be played (Let's imagine it like music CD player)*/
             Sequencer player = MidiSystem.getSequencer();
             player.open();
+
+            /*The Sequence is the song, the musical piece that the Sequencer will play.
+            (Let's imagine it like a CD, but it plays only one song)*/
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+
+            /* Track is the place, where all the songs lives*/
+            Track track = seq.createTrack();
+
+
         } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        } catch (InvalidMidiDataException e) {
             e.printStackTrace();
         }
     }
