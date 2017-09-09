609d59b2c06209d6f4a50e397a940e4c9baa92bf
Vladyslove <od.vladyslav@gmail.com>
2017-09-09 10:50:01 +0300

    Making a color by passing in 3 ints to represent RGB values

--- src/main/java/MyDrawPanel.java
+++ src/main/java/MyDrawPanel.java
@@ -20,7 +20,14 @@ public class MyDrawPanel extends JPanel {
     public void paintComponent3 (Graphics g) {
 
         g.fillRect(0,0,this.getWidth(),this.getHeight());
-        
+
+        int red = (int) (Math. random() * 255);
+        int green = (int) (Math. random() * 255);
+        int blue = (int) (Math. random() * 255);
+
+        Color randomColor = new Color(red, green, blue);
+        g.setColor(randomColor);
+        g.fillOval(65,65, 96,96);
     }
 
 
