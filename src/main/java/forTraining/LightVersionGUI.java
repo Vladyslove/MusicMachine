be597fd0986f628284630b8e1bc2acaa3a3c0ff1
Vladyslove <od.vladyslav@gmail.com>
2017-09-10 15:41:12 +0300

    Deleted unnecessaryis methods. Added Light Version of GUI with method go(), that allows to change colors.

---
+++ src/main/java/forTraining/LightVersionGUI.java
@@ -0,0 +1,29 @@
+package forTraining;
+
+import oracle.jrockit.jfr.JFR;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by User on 07.09.2017.
+ */
+public class LightVersionGUI implements ActionListener{
+    JFrame frame;
+
+    public static void main(String[] args) {
+        LightVersionGUI LVgui = new LightVersionGUI();
+        LVgui.go();
+    }
+
+    private void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Change colors");
+        button.addActionListener(this);
+
+        button.addActionListener(this);
+
+}
