3f6d5aa99fec9eb2a8e1f2e8ef3fbd4a28b6e228
Vladyslove <od.vladyslav@gmail.com>
2017-09-10 15:23:38 +0300

    new version of paintComponent method is like the previous, except it makes random colors for tge start and end colors of the gradient

--- src/main/java/forTesting/MyDrawPanel.java
+++ src/main/java/forTesting/MyDrawPanel.java
@@ -42,7 +42,24 @@ public class MyDrawPanel extends JPanel {
         g2d.setPaint(gradient);
 
         g2d.fillOval(65,65, 95,95);
+    }
 
+    public void paintComponent5 (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        int red = (int) (Math. random() * 255);
+        int green = (int) (Math. random() * 255);
+        int blue = (int) (Math. random() * 255);
+        Color startColor = new Color(red, green, blue);
+
+        red = (int) (Math. random() * 255);
+        green = (int) (Math. random() * 255);
+        blue = (int) (Math. random() * 255);
+        Color endColor = new Color(red, green, blue);
+
+        GradientPaint gradient = new GradientPaint(66,66,startColor, 146,146, endColor);
+        g2d.setPaint(gradient);
+        g2d.fillOval(66,66,96,96);
     }
 
 
