a7926e9e84ac871716c5ed418604d302ea803017
Vladyslove <od.vladyslav@gmail.com>
2017-09-12 00:40:04 +0300

    nothing new here. Widgets are made and put in the frame

--- src/main/java/forTraining/simpleAnimation/SimpleAnimation.java
+++ src/main/java/forTraining/simpleAnimation/SimpleAnimation.java
@@ -11,6 +11,22 @@ public class SimpleAnimation {
     int x = 70;
     int y = 70;
 
+    public static void main(String[] args) {
+        SimpleAnimation gui = new SimpleAnimation();
+        gui.go();
+
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(drawPanel);
+        frame.setSize(275, 275);
+        frame.setVisible(true);
+    }
 
 
     class MyDrawPanel extends JPanel {
