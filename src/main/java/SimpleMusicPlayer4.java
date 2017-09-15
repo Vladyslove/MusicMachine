9f424967c407ffb3e6581a0605eaf2b0d5bfdd81
Vladyslove <od.vladyslav@gmail.com>
2017-09-15 16:43:47 +0300

    For previous commit: checkboxes stores in ArrayList. Added array of instrument names

---
+++ src/main/java/SimpleMusicPlayer4.java
@@ -0,0 +1,24 @@
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * Created by User on 15.09.2017.
+ */
+public class SimpleMusicPlayer4 {
+
+    JPanel mainPanel;
+    ArrayList<JCheckBox> chechBoxList;
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+    JFrame frame;
+
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-Bat", "Open Hi-Hat",
+            "Acoustic Snare", "Crash Cymbal", "Band Clap",
+            "H1gh Tom", "Hi Bongo", "Maxacas", "Whistle", "Low Conga",
+            "Cowbell", "Vibraslap", "Low-mid Tom", "High Aqoqo" , "0pQn Hi Conga"};
+    
+}
