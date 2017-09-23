ec24097a7122ad1eefcf0004fe3d65b240fe93ec
Vladyslove <od.vladyslav@gmail.com>
2017-09-23 18:16:42 +0300

    I'm gonna implement method go()

--- src/main/java/forTraining/simpleMusicPlayer6/SimpleChatClient.java
+++ src/main/java/forTraining/simpleMusicPlayer6/SimpleChatClient.java
@@ -1,9 +1,27 @@
 package forTraining.simpleMusicPlayer6;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  * Created by User on 23.09.2017.
  */
 public class SimpleChatClient {
 
-    
+    JTextArea incoming;
+    JTextField outgoing;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+
+    public static void main(String[] args) {
+        SimpleChatClient client = new SimpleChatClient();
+        client.go();
+    }
+
+    private void go() {
+        
+    }
 }
