a4431fe4f6d40f80655040f845cd6ddad1858488
Vladyslove <od.vladyslav@gmail.com>
2017-09-25 23:17:09 +0300

    Socket is using for getting the input and output streams. And the new 'reader' can get messages from the server

--- src/main/java/forTraining/simpleMusicPlayer6/SimpleChatClient.java
+++ src/main/java/forTraining/simpleMusicPlayer6/SimpleChatClient.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -58,7 +59,10 @@ public class SimpleChatClient {
     private void setUpNetworking() {
         try {
             sock = new Socket("127.0.0.1", 5000);
-            
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("networking established");
         } catch (IOException e) {
             e.printStackTrace();
         }
