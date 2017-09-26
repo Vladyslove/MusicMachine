75de4e3b115bbd2556d640b2d097d74045727fea
Vladyslove <od.vladyslav@gmail.com>
2017-09-26 18:54:26 +0300

    Added inner class for getting client stream

--- src/main/java/forTraining/simpleMusicPlayer6/VerySimpleChatServer.java
+++ src/main/java/forTraining/simpleMusicPlayer6/VerySimpleChatServer.java
@@ -1,8 +1,44 @@
 package forTraining.simpleMusicPlayer6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+
 /**
  * Created by User on 26.09.2017.
  */
 public class VerySimpleChatServer {
-    
+    ArrayList clientOutputStreams;
+
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket sock;
+        
+        public ClientHandler(Socket clientSocket) {
+            sock = clientSocket;
+            try {
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    tellEveryone(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void tellEveryone(String message) {
+    }
 }
