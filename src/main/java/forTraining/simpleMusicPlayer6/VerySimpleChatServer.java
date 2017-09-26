2e841e66b4465c4f28da369109a062ab7ae321db
Vladyslove <od.vladyslav@gmail.com>
2017-09-26 20:09:41 +0300

    Added method go, which make connection beetweeen server and client, and also write an outputstream

--- src/main/java/forTraining/simpleMusicPlayer6/VerySimpleChatServer.java
+++ src/main/java/forTraining/simpleMusicPlayer6/VerySimpleChatServer.java
@@ -3,6 +3,8 @@ package forTraining.simpleMusicPlayer6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -39,6 +41,30 @@ public class VerySimpleChatServer {
         }
     }
 
+    public static void main(String[] args) {
+        new VerySimpleChatServer().go();
+    }
+
+    private void go() {
+        clientOutputStreams = new ArrayList();
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+                System.out.println("got a connection");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void tellEveryone(String message) {
+
     }
 }
